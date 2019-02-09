5ef6e87650689812395011431fe7c67f6516d0cc
Chris Stowe <chstowe@gmail.com>
2019-02-09 15:00:23 -0800

    Add new step for generating the deploy config

---
+++ deploy/create-deploy-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ${CIRCLE_BRANCH} = 'master' ]; then
+    ENV=prod
+else
+    ENV=dev
+fi
+
+envsubst < ./deploy/template.deployment.yaml > ./config/deployment.yaml
+envsubst < ./deploy/template.service.yaml > ./config/service.yaml
+envsubst < ./deploy/template.ingress.yaml > ./config/ingress.yaml
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-if [ ${CIRCLE_BRANCH} = 'master' ]; then
-    ENV=prod
-else
-    ENV=dev
-fi
-
 gcloud config set project forgestatus
-
 echo ${GCP_SERVICE_ACCOUNT} > gcp-service-account.json
-
 gcloud auth activate-service-account --key-file gcp-service-account.json
-
 gcloud container clusters get-credentials forgestatus-cluster --zone us-west2-a
 
-envsubst < ./deploy/deployment.in.yaml > ./deploy/deployment.out.yaml
-kubectl apply -f ./deploy/deployment.out.yaml
-
-envsubst < ./deploy/service.in.yaml > ./deploy/service.out.yaml
-kubectl apply -f ./deploy/service.out.yaml
-
-envsubst < ./deploy/ingress.in.yaml > ./deploy/ingress.out.yaml
-kubectl apply -f ./deploy/ingress.out.yaml
+kubectl apply -f ./config/deployment.yaml
+kubectl apply -f ./config/service.yaml
+kubectl apply -f ./config/ingress.yaml
