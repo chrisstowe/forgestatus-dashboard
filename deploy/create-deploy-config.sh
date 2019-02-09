c6e23ba8a128e92782971dddb6bb9df93e451fa3
Chris Stowe <chstowe@gmail.com>
2019-02-09 15:02:41 -0800

    Make config dir

--- deploy/create-deploy-config.sh
+++ deploy/create-deploy-config.sh
@@ -6,6 +6,8 @@ else
     ENV=dev
 fi
 
+mkdir config
+
 envsubst < ./deploy/template.deployment.yaml > ./config/deployment.yaml
 envsubst < ./deploy/template.service.yaml > ./config/service.yaml
 envsubst < ./deploy/template.ingress.yaml > ./config/ingress.yaml
