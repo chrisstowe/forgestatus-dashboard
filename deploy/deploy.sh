5a5ee593004a396db67a1fcea7335b8421e8c8b8
Chris Stowe <chstowe@gmail.com>
2019-02-09 14:21:32 -0800

    Update deploy script to apply config to k8s

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-echo deploying
+set -x
+set -e
+set -o allexport
+
+if [ ${CIRCLE_BRANCH} = 'master' ]; then
+    ENV=prod
+else
+    ENV=dev
+fi
+
+gcloud config set project forgestatus
+
+echo ${GCP_SERVICE_ACCOUNT} > gcp-service-account.json
+
+gcloud auth activate-service-account --key-file gcp-service-account.json
+
+gcloud container clusters get-credentials forgestatus-cluster
+
+envsubst < ./deploy/deployment.in.yaml > ./deploy/deployment.out.yaml
+kubectl apply -f ./deploy/deployment.out.yaml
+
+envsubst < ./deploy/service.in.yaml > ./deploy/service.out.yaml
+kubectl apply -f ./deploy/service.out.yaml
+
+envsubst < ./deploy/ingress.in.yaml > ./deploy/ingress.out.yaml
+kubectl apply -f ./deploy/ingress.out.yaml
