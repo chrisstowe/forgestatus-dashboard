cda7f71ac7e37b2124a1b4d69625f1a87a6da246
Chris Stowe <chstowe@gmail.com>
2019-02-09 21:35:57 -0800

    Add app element and unstub test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <h1>ForgeStatus Dashboard</h1>
+        <p>Active work is in progress, please check back soon.</p>
+        <h2>ForgeStatus Repo</h2>
+        <a href="https://github.com/chrisstowe/forgestatus">https://github.com/chrisstowe/forgestatus</a>
+        <br />
+        <h2>ForgeStatus Dashboard Repo</h2>
+        <a href="https://github.com/chrisstowe/forgestatus-dashboard">https://github.com/chrisstowe/forgestatus-dashboard</a>
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>ForgeStatus Dashboard</h1>, document.getElementById('root'));
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
