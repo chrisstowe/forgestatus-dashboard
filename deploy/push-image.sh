022ca4f8f964625ec5b9aeb4a88c41fd8b2428a6
Chris Stowe <chstowe@gmail.com>
2019-02-10 14:03:22 -0800

    Consolidate pushing the image to one script

--- deploy/push-image.sh
+++ deploy/push-image.sh
@@ -1,5 +1,13 @@
 #!/bin/bash          
 
+gcloud config set project forgestatus
+
+echo ${GCP_SERVICE_ACCOUNT} > gcp-service-account.json
+
+gcloud auth activate-service-account --key-file gcp-service-account.json
+
+gcloud docker -- build -t gcr.io/forgestatus/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1} -f Dockerfile .
+
 gcloud docker -- push gcr.io/forgestatus/${CIRCLE_PROJECT_REPONAME}:${CIRCLE_SHA1}
 
 if [ ${CIRCLE_BRANCH} = 'master' ]; then
