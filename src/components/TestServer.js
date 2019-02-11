68dc024c50440af8b42f8fcd9ea3894ad0e83250
Chris Stowe <chstowe@gmail.com>
2019-02-10 19:35:23 -0800

    Update ingress to allow routing to individual workers

--- src/components/TestServer.js
+++ src/components/TestServer.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import request from 'superagent';
 
-const handleTestServer = async () => {
+const handleTestClicked = async (serviceName) => {
     try {
         const resp = await request
-            .get('/api/getServerStatus')
-            .timeout({ response: 2000 });
-        alert(`status=${resp.status}: ${resp.text}`);
-    } catch(err) {
-        alert(`err=${JSON.stringify(err)}`);
-    }
-}
-
-const handleTestWorker = async () => {
-    try {
-        const resp = await request
-            .get('/api/getWorkerStatus')
+            .get(`${serviceName}/status`)
             .timeout({ response: 2000 });
         alert(`status=${resp.status}: ${resp.text}`);
     } catch(err) {
@@ -25,10 +14,10 @@ const handleTestWorker = async () => {
 
 const TestServer = () => (
     <div>
-        <button onClick={() => handleTestServer()}>
+        <button onClick={() => handleTestClicked('server')}>
             Test Server
         </button>
-        <button onClick={() => handleTestWorker()}>
+        <button onClick={() => handleTestClicked('worker1')}>
             Test Worker
         </button>
     </div>
