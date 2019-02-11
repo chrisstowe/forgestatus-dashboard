41bdaaefde7f75a85432ad30dd707c6984ab37d7
Chris Stowe <chstowe@gmail.com>
2019-02-10 17:13:24 -0800

    Simplify test request even more

--- src/components/TestServer.js
+++ src/components/TestServer.js
@@ -4,9 +4,8 @@ import request from 'superagent';
 const handleTestServer = async () => {
     try {
         const resp = await request
-            .get(`${window.location.hostname}/api/getStatus`)
+            .get('/api/getStatus')
             .timeout({ response: 2000 });
-
         alert(`status=${resp.status} body=${JSON.stringify(resp)}`);
     } catch(err) {
         alert(`err=${JSON.stringify(err)}`);
