4fc69470d62c68731561640d3f6d0c06a3abeb72
Chris Stowe <chstowe@gmail.com>
2019-02-10 18:52:54 -0800

    Add test worker button

--- src/components/TestServer.js
+++ src/components/TestServer.js
@@ -4,7 +4,18 @@ import request from 'superagent';
 const handleTestServer = async () => {
     try {
         const resp = await request
-            .get('/api/getStatus')
+            .get('/api/getServerStatus')
+            .timeout({ response: 2000 });
+        alert(`status=${resp.status}: ${resp.text}`);
+    } catch(err) {
+        alert(`err=${JSON.stringify(err)}`);
+    }
+}
+
+const handleTestWorker = async () => {
+    try {
+        const resp = await request
+            .get('/api/getWorkerStatus')
             .timeout({ response: 2000 });
         alert(`status=${resp.status}: ${resp.text}`);
     } catch(err) {
@@ -13,9 +24,14 @@ const handleTestServer = async () => {
 }
 
 const TestServer = () => (
-    <button onClick={() => handleTestServer()}>
-        Test Server
-    </button>
+    <div>
+        <button onClick={() => handleTestServer()}>
+            Test Server
+        </button>
+        <button onClick={() => handleTestWorker()}>
+            Test Worker
+        </button>
+    </div>
 );
 
 export default TestServer;
