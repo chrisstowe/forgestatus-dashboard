ffa28d3c98c4a9b2e866bd2950cf14bf678fc274
Chris Stowe <chstowe@gmail.com>
2019-02-21 14:52:03 -0800

    Add a demo change to the main page

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import './app.css';
 
 const App = () => (
     <div className="demo">
+        <h1>Hello ForgeRock</h1>
         <h1>ForgeStatus Dashboard</h1>
         <SimpleLineChart />
         <p>Active work is in progress, please check back soon.</p>
