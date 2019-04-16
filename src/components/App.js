f76947fc6fc70990402bd986b3a7656ccd2c5b99
Chris Stowe <chstowe@gmail.com>
2019-04-15 21:10:35 -0700

    Make minor change to test demo

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import './app.css';
 
 const App = () => (
     <div className="demo">
-        <h1>ForgeStatus Dashboard</h1>
+        <h1>ForgeStatus Dashboard Demo</h1>
         <SimpleLineChart />
         <p>Active work is in progress, please check back soon.</p>
         <h2>ForgeStatus Repo</h2>
